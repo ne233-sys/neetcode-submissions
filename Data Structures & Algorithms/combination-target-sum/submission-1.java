4b826bd21fe3661c0f46b2d7a4e5d5f2ba26c3a7
Nikita Efremov <ne233@njit.edu>
2026-08-11 09:58:27 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    private List<List<Integer>> result;
+    private int[] nums;
+    private int target;
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        result = new ArrayList<>();
+        this.nums = nums;
+        this.target = target;
+
+        List<Integer> subset = new ArrayList<>();
+        rec(subset, 0, 0);
+
+        return result;
+    }
+
+    private void rec(List<Integer> subset, int sum, int i) {
+        if(sum < target) {
+            List<Integer> copy = new ArrayList<>(subset);
+            copy.add(nums[i]);
+
+            if(sum + nums[i] <= target) rec(copy, sum + nums[i], i);
+            if(i + 1 < nums.length) rec(subset, sum, i + 1);  
+        }
+        else if(sum == target) result.add(subset);
+    }
+}
