94078963e4ec2f7b75950923424376491d56d8b3
Nikita Efremov <ne233@njit.edu>
2026-08-12 17:20:09 -0400

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        int min = 200;
+        char c;
+
+        for(String s : strs) if(s.length() < min) min = s.length();
+
+        for(int i = 0; i < min; i++){
+            c = strs[0].charAt(i);
+
+            for(String s : strs){
+                if(s.charAt(i) != c) return result; 
+            }
+
+            result += c;
+        }
+
+        return result;
+    }
+}
