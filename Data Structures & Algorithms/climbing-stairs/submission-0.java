9e646036c64b690dc20f3b3b8bdbad66dc714e11
Nikita Efremov <ne233@njit.edu>
2026-08-14 15:44:37 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int[] stairs = new int[n];
+        stairs[0] = 1;
+        stairs[1] = 2;
+
+        for(int i = 2; i < n; i++) stairs[i] = stairs[i - 2] + stairs[i - 1];
+        return stairs[n-1];
+    }
+}
