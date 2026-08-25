e720ce1898dfd7fd0ca40863eae46a7cd4d8f2f9
Nikita Efremov <ne233@njit.edu>
2026-08-25 16:16:12 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> post = new HashMap<>();
+        Map<Integer, Stack<Integer>> pre = new HashMap<>();
+        Queue<Integer> que = new ArrayDeque<>();
+        Set<Integer> flag = new HashSet<>();
+
+        for(int[] pair : prerequisites){
+            if(pair[0] == pair[1]) return false;
+
+            if(!post.containsKey(pair[0])) post.put(pair[0], new HashSet<>());
+            post.get(pair[0]).add(pair[1]);
+        }
+
+        for(int[] pair : prerequisites){
+            if(!pre.containsKey(pair[1])) pre.put(pair[1], new Stack<>());
+            pre.get(pair[1]).push(pair[0]);
+        }
+
+        for(int i = numCourses - 1; i >= 0; i--) if(!post.containsKey(i)){
+            if(pre.containsKey(i)) que.add(i);
+            else numCourses--;
+        }
+
+        while(!que.isEmpty()) {
+            numCourses--;
+            int curr = que.remove();
+
+            if(flag.contains(curr)) return false;
+            else flag.add(curr);
+
+            if(!pre.containsKey(curr)) continue;
+            
+            Stack<Integer> stack = pre.get(curr);
+
+            while(!stack.isEmpty()){
+                int course = stack.pop();
+                Set<Integer> set = post.get(course);
+
+                set.remove(curr);
+                if(set.isEmpty()) que.add(course);
+            }
+        }
+
+        if(numCourses == 0) return true;
+        else return false;
+    }
+}
