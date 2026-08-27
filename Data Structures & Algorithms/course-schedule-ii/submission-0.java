35cb59809dc11d4f6b8b4d82f8836a1f30fe9c90
Nikita Efremov <ne233@njit.edu>
2026-08-26 21:15:41 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> post = new HashMap<>();
+        Map<Integer, Stack<Integer>> pre = new HashMap<>();
+        Queue<Integer> que = new ArrayDeque<>();
+        int[] result = new int[numCourses];
+        int last = 0;
+
+        for(int[] pair : prerequisites){
+            if(pair[0] == pair[1]) return new int[0];
+
+            if(!post.containsKey(pair[0])) post.put(pair[0], new HashSet<>());
+            post.get(pair[0]).add(pair[1]);
+        }
+
+        for(int[] pair : prerequisites){
+            if(!pre.containsKey(pair[1])) pre.put(pair[1], new Stack<>());
+            pre.get(pair[1]).push(pair[0]);
+        }
+
+        for(int i = numCourses - 1; i >= 0; i--) if(!post.containsKey(i)){
+            if(pre.containsKey(i)) que.add(i);
+            else {
+                result[last] = i;
+                last++;
+            }
+        }
+
+        while(!que.isEmpty()) {
+            int curr = que.remove();
+            result[last] = curr;
+            last++;
+            if(!pre.containsKey(curr)) continue;
+            Stack<Integer> stack = pre.get(curr);
+
+            while(!stack.isEmpty()){
+                int course = stack.pop();
+                Set<Integer> set = post.get(course);
+
+                set.remove(curr);
+                if(set.isEmpty()) que.add(course);
+            }
+        }
+
+        if(last == numCourses) return result;
+        else return new int[0];
+    }
+}
