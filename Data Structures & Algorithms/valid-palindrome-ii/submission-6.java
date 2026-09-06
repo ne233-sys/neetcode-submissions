80b56feaf77e1c6845f5a22d714875f1f3c07365
Nikita Efremov <ne233@njit.edu>
2026-09-06 15:09:08 -0400

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        boolean once = false;
+        int l = 0, r = s.length() - 1;
+
+        while(l + 1 < r){
+            if(s.charAt(l) != s.charAt(r)){
+                if(once) return false;
+
+                if(s.charAt(l+1) == s.charAt(r) && s.charAt(l+2) == s.charAt(r-1)) l++;
+                else if (s.charAt(l) == s.charAt(r-1) && s.charAt(l+1) == s.charAt(r-2)) r--;
+                else return false;
+
+                once = true;
+            }
+
+            l++;
+            r--;
+        }
+
+        if(r + 1 == l && once && s.charAt(l) != s.charAt(r)) return false;
+
+        return true;
+    }
+}
